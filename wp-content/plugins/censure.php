2e1a33ac3dd7252b1f61c7e4850424d039697d43
VikaShepilo <shepilovika@gmail.com>
2022-03-09 00:04:12 +0300

    fixed plagin censure

--- wp-content/plugins/censure.php
+++ wp-content/plugins/censure.php
@@ -5,7 +5,53 @@ Description: Comment censorship
 Author: Shepilo Vika
 */
 
-function censure_filter_comments($the_content)
+function myplugin_activation(){
+	create_table();
+}
+register_activation_hook( __FILE__, 'myplugin_activation' );
+
+function myplugin_deactivation(){
+    global $wpdb; 
+    $table_name = $wpdb->prefix . 'cens'; 
+    $sql = "DROP TABLE IF EXISTS $table_name"; 
+    $wpdb->query($sql); 
+    delete_option("my_plugin_db_version");
+}
+register_deactivation_hook( __FILE__, 'myplugin_deactivation' );
+
+function create_table(){
+    global $wpdb;
+    $table_name = $wpdb->prefix . "cens";
+    $sql = "CREATE TABLE " . $table_name . " (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        cense text NOT NULL COLLATE utf8_general_ci,
+        UNIQUE KEY id (id)
+    );";
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+} 
+add_action( 'init', 'create_table' );
+
+function menu_panel_add_word() 
+{
+    add_menu_page('Add word', 'Add word', 'manage_options', 'add_word.php', 'add_word','dashicons-phone', 1);
+    function add_word() {
+    echo "<h2>Add word</h2>
+    <hr>
+        <p>
+            <form method='POST' action=''>
+                New word: 
+                <input type='text' name='new-word'><br>
+                <input type='submit'>
+                </p>
+            </form>
+        </p>
+    <hr>";
+    }
+}
+add_action('admin_menu', 'menu_panel_add_word');
+
+function censure_filter_comments($comment_text)
 {
     global $wpdb;
     $results = $wpdb->get_results( "SELECT cense FROM {$wpdb->prefix}cens", OBJECT );
@@ -15,20 +61,20 @@ function censure_filter_comments($the_content)
             array_push($obscene_words, $value);
         }
     }
-
-    $the_censure_content = str_ireplace(array_values($obscene_words), "***", $the_content);
-
+    $the_censure_content = str_ireplace(array_values($obscene_words), "***", $comment_text);
     return  $the_censure_content;
 }
-add_filter('the_content', 'censure_filter_comments');
+add_filter('comment_text', 'censure_filter_comments');
 
-
-function censure_filter_data( $data ) 
+function censure_filter_data() 
 {
     global $wpdb;
-    $wpdb->insert("{$wpdb->prefix}cens",  array(
-        'cense' => $data['new-word'],
-    ));
+    if (isset($_POST['new-word'])) {
+        $data = $_POST['new-word'];
+        $wpdb->insert("{$wpdb->prefix}cens",  array(
+        'id' => 'AUTO_INCREMENT',
+        'cense' => $data,
+        ));
+    }
 } 
-add_filter( 'wpcf7_posted_data', 'censure_filter_data'); 
-
+add_filter('admin_init', 'censure_filter_data'); 
